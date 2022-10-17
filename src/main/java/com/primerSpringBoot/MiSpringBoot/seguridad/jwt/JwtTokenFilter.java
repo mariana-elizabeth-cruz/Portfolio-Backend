70f309a5f11dc0f001878fd80c1494b0e9e60868
meryeliz <lizzybtk@outlook.com>
2022-10-17 00:26:34 -0300

    cambios tokenfilter

--- src/main/java/com/primerSpringBoot/MiSpringBoot/seguridad/jwt/JwtTokenFilter.java
+++ src/main/java/com/primerSpringBoot/MiSpringBoot/seguridad/jwt/JwtTokenFilter.java
@@ -45,7 +45,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     }
     
     private String getToken(HttpServletRequest request){
-        String header = request.getHeader("Autorizacion");
+        String header = request.getHeader("Authorization");
         if(header != null && header.startsWith("Bearer."))
             return header.replace("Bearer","");
         return null;
